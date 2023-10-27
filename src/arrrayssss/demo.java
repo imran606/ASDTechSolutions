8300cf2879ff764f201b0cc8511e8e5b58c00a65
Hamid Hussain <Hamid Hussain@DESKTOP-0IS7DQ4>
2023-10-27 11:29:38 +0530

    this is imran

---
+++ src/arrrayssss/demo.java
@@ -0,0 +1,14 @@
+package arrrayssss;
+
+public class demo
+{
+	public static void main(String[] args) 
+	{
+	 	int a =800;
+				if(a>=500) 
+				{
+					System.out.println("10 rupees discount");
+				}
+	}
+
+}
---
+++ src/arrrayssss/demo1.java
@@ -0,0 +1,18 @@
+package arrrayssss;
+
+public class demo1 
+{
+	public static void main(String[] args)
+	{
+		if((!true)||(!false))
+		{
+			System.out.println("hiiiii");
+		}else
+			
+		{
+			System.out.println("hello");
+		}
+		
+	}
+
+}
